6da45c167f1c3864a2591c086291f6c8ffe1d5e4
selfie <selfie@alookso.com>
2022-09-17 01:35:11 +0900

    Create main.py
    One script to rule them all,

---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+from src.train import train, test
+
+parser = argparse.ArgumentParser(description='sommelier transformer',
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-M', '--mode', default='test', choices=['train', 'test'], dest='mode', help='train or test.')
+args = parser.parse_args()
+
+if __name__=='__main__':
+    mode = args.mode
+    
+    if mode=='train':
+        train()
+    elif mode=='test':
+        test()
