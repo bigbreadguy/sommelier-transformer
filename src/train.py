2e050fd800d6641590924b241c6ad59b3eba23b4
selfie <selfie@alookso.com>
2022-09-17 01:10:51 +0900

    Update train.py
    Fix train function, add test function, fix quotes to meet the convention

--- src/train.py
+++ src/train.py
@@ -25,8 +25,8 @@ def train():
     train_test_split_ratio = 0.2
     warmup_steps = 1e4
     batch_size = 4
-    encoder_max_len = 250
-    decoder_max_len = 54
+    encoder_max_len = 54
+    decoder_max_len = 250
     buffer_size = 1000
 
     data_dir = os.path.join(os.getcwd(), 'data')
@@ -51,16 +51,16 @@ def train():
     valid_steps = int(np.ceil(nvalid/batch_size))
 
     tokenizer = tokenizer()
-    train_ds = train_dataset.map(lambda x: encode(x, tokenizer=tokenizer))
-    valid_ds = valid_dataset.map(lambda x: encode(x, tokenizer=tokenizer))
+    train_ds = train_dataset.map(lambda x: encode(x, tokenizer=tokenizer, encoder_max_len=encoder_max_len, decoder_max_len=decoder_max_len))
+    valid_ds = valid_dataset.map(lambda x: encode(x, tokenizer=tokenizer, encoder_max_len=encoder_max_len, decoder_max_len=decoder_max_len))
 
     tf_train_ds = to_tf_dataset(train_ds)
     tf_valid_ds = to_tf_dataset(valid_ds)
 
     tf_train_ds = create_dataset(tf_train_ds, batch_size=batch_size,
-                        shuffling=True, cache_path = None)
+                        shuffling=True, cache_path=cache_path_train, buffer_size=buffer_size)
     tf_valid_ds = create_dataset(tf_valid_ds, batch_size=batch_size,
-                        shuffling=False, cache_path = None)
+                        shuffling=False, cache_path=cache_path_test, buffer_size=buffer_size)
 
     start_profile_batch = steps + 10
     stop_profile_batch = start_profile_batch + 100
@@ -82,13 +82,38 @@ def train():
     callbacks = [tensorboard_callback, model_checkpoint_callback] 
     metrics = [tf.keras.metrics.SparseTopKCategoricalAccuracy(name='accuracy')]
 
-    learning_rate = CustomSchedule()
+    learning_rate = CustomSchedule(warmup_steps=warmup_steps)
     optimizer = tf.keras.optimizers.Adam(learning_rate)
 
-    model = WineReviewT5.from_pretrained("t5-base")
+    model = WineReviewT5.from_pretrained('t5-base')
     model.compile(optimizer=optimizer, metrics=metrics)
 
     epochs_done = 0
     model.fit(tf_train_ds, epochs=5, steps_per_epoch=steps, callbacks=callbacks, 
                validation_data=tf_valid_ds, validation_steps=valid_steps, initial_epoch=epochs_done)
     model.save_pretrained(save_path)
+
+def test():
+    encoder_max_len = 54
+    decoder_max_len = 250
+    save_path = os.path.join(os.getcwd(), 'data', 'experiments', 't5', 'models')
+
+    tokenizer = tokenizer()
+    model = WineReviewT5.from_pretrained(save_path)
+
+    province = input('Province, ex) Burgundy : ')
+    variety = input('Grape Variety, ex) Chardonnay : ')
+    points = input('How would you rate the wine, Scale 0 to 100? : ')
+    input_text = f'review_me: {str(variety)} from {str(province)}, point {str(points)} </s>'
+
+    encoded_query = tokenizer(input_text,
+                        return_tensors='tf', pad_to_max_length=True, truncation=True, max_length=encoder_max_len)
+    input_ids = encoded_query['input_ids']
+    attention_mask = encoded_query['attention_mask']
+    
+    
+    generated_answer = model.generate(input_ids, attention_mask=attention_mask,
+                                    max_length=decoder_max_len, top_p=0.95, top_k=50, repetition_penalty=2)
+    decoded_answer = tokenizer.decode(generated_answer.numpy()[0])
+
+    print('Review: ', decoded_answer)
