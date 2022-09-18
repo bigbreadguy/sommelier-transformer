59dcc0b8b63c19b0e0e0ca3201dd63240efec9bc
selfie <selfie@alookso.com>
2022-09-18 10:12:28 +0900

    Update train.py
    Giving specified steps would cause an error, setting it to none let the framework calculate proper steps

--- src/train.py
+++ src/train.py
@@ -103,8 +103,8 @@ def train(shards=False):
     model.compile(optimizer=optimizer, metrics=metrics)
 
     epochs_done = 0
-    model.fit(tf_train_ds, epochs=5, steps_per_epoch=20000, callbacks=callbacks,
-               validation_data=tf_valid_ds, validation_steps=valid_steps, initial_epoch=epochs_done)
+    model.fit(tf_train_ds, epochs=5, steps_per_epoch=None, callbacks=callbacks,
+               validation_data=tf_valid_ds, validation_steps=None, initial_epoch=epochs_done)
     model.save_pretrained(save_path)
 
 def test():
